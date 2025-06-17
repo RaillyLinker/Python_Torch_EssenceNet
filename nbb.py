9ce51bb2f03cc185fa232869675c313ef6171569
RaillyLinker <raillylinker@gmail.com>
2025-06-17 20:20:07 +0900

    update : depthwise 적용

--- nbb.py
+++ nbb.py
@@ -10,27 +10,27 @@ class EssenceNet(nn.Module):
         self.comp_feat_blocks = nn.ModuleList([
             # 3x3 픽셀 단위 특징 검출
             # 정보가 혼재된 노이즈 신호
-            self._double_conv_block(1, 32, 8, 3, 2, 1),  # 320x320 -> 160x160
+            self._double_conv_block(1, 32, 16, 3, 2, 1),  # 320x320 -> 160x160
             # 7x7 픽셀 단위 특징 검출
             # 노이즈 + 점의 의미
-            self._double_conv_block(8, 32, 8, 3, 2, 1),  # 160x160 -> 80x80
+            self._depthwise_separable_conv_block(16, 64, 3, 2, 1),  # 160x160 -> 80x80
             # 15x15 픽셀 단위 특징 검출
             # 점 + 작은 단위 선
-            self._double_conv_block(8, 64, 16, 3, 2, 1),  # 80x80 -> 40x40
+            self._depthwise_separable_conv_block(64, 96, 3, 2, 1),  # 80x80 -> 40x40
             # 31x31 픽셀 단위 특징 검출
             # 점 + 자유로운 선 + 최소 단위 도형
-            self._double_conv_block(16, 64, 16, 3, 2, 1),  # 40x40 -> 20x20
+            self._depthwise_separable_conv_block(96, 128, 3, 2, 1),  # 40x40 -> 20x20
             # 63x63 픽셀 단위 특징 검출
             # 점 + 자유로운 선 + 자유로운 도형
-            self._double_conv_block(16, 128, 32, 3, 2, 1),  # 20x20 -> 10x10
+            self._depthwise_separable_conv_block(128, 160, 3, 2, 1),  # 20x20 -> 10x10
             # 127x127 픽셀 단위 특징 검출
             # 점 + 자유로운 선 + 자유로운 도형 + 질감
             # 실사 이미지 표현이 가능한 영역이며, 아래부터는 추상적 정보
-            self._double_conv_block(32, 128, 32, 3, 2, 1),  # 10x10 -> 5x5
+            self._depthwise_separable_conv_block(160, 224, 3, 2, 1),  # 10x10 -> 5x5
             # 255x255 픽셀 단위 특징 검출
-            self._double_conv_block(32, 256, 64, 3, 2, 1),  # 5x5 -> 3x3
+            self._depthwise_separable_conv_block(224, 256, 3, 2, 1),  # 5x5 -> 3x3
             # 320x320 픽셀 단위 특징 검출
-            self._double_conv_block(64, 256, 64, 3, 1, 0)  # 3x3 -> 1x1
+            self._depthwise_separable_conv_block(256, 512, 3, 1, 0)  # 3x3 -> 1x1
         ])
 
     def _single_conv_block(self, in_ch, out_ch, ks, strd, pdd):
@@ -50,6 +50,21 @@ class EssenceNet(nn.Module):
 
             # 채널간 패턴 분석
             nn.Conv2d(mid_ch, out_ch, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(out_ch),
+            nn.SiLU()
+        )
+
+    # Ghost-Net 방식으로 핵심 정보를 가진 첫째 레이어에서부터 시작해서 다음으로는 Depthwise-Pointwise 구조로 정보 추출
+    # todo : 만약 이 방식의 정확도가 나오지 않는다면 전체 double_conv_block 적용(경량화 필요)
+    def _depthwise_separable_conv_block(self, in_ch, out_ch, ks, strd, pdd):
+        return nn.Sequential(
+            # Depthwise convolution
+            nn.Conv2d(in_ch, in_ch, kernel_size=ks, stride=strd, padding=pdd, groups=in_ch, bias=False),
+            nn.BatchNorm2d(in_ch),
+            nn.SiLU(),
+
+            # Pointwise convolution (1x1)
+            nn.Conv2d(in_ch, out_ch, kernel_size=1, stride=1, padding=0, bias=False),
             nn.BatchNorm2d(out_ch),
             nn.SiLU()
         )
