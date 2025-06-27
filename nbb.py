84d7d0dfcbeb890c18901f9cf1269029acd0c135
RaillyLinker <raillylinker@gmail.com>
2025-06-27 15:02:26 +0900

    update : essence net 을 근본으로 되돌림

--- nbb.py
+++ nbb.py
@@ -1,177 +1,81 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torchvision.ops import StochasticDepth
 
 
-class SEBlock(nn.Module):
-    def __init__(self, channels):
-        super(SEBlock, self).__init__()
-        self.channels = channels
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-
-        hidden_dim = channels // 4
-        self.fc = nn.Sequential(
-            nn.Linear(channels, hidden_dim, bias=False),
-            nn.SiLU(),
-            nn.Linear(hidden_dim, channels, bias=False),
-            nn.Sigmoid()
-        )
-
-    def forward(self, x):
-        b, c, _, _ = x.size()
-        y = self.avg_pool(x).view(b, c)  # (B, C)
-        y = self.fc(y).view(b, c, 1, 1)  # (B, C, 1, 1)
-        return x * y
-
-
-def _double_conv_block(in_ch, mid_ch, out_ch, ks, strd, pdd):
+def _single_conv_block(in_ch, out_ch, ks, strd, pdd):
     return nn.Sequential(
         # 평면당 형태를 파악
-        nn.Conv2d(in_ch, mid_ch, kernel_size=ks, stride=strd, padding=pdd, bias=False),
-        nn.BatchNorm2d(mid_ch),
-        nn.SiLU(),
-
-        # 채널간 패턴 분석
-        nn.Conv2d(mid_ch, out_ch, kernel_size=1, stride=1, padding=0, bias=False),
+        nn.Conv2d(in_ch, out_ch, kernel_size=ks, stride=strd, padding=pdd, bias=False),
         nn.BatchNorm2d(out_ch),
         nn.SiLU()
     )
 
 
-def _mb_conv_block(in_ch, mid_ch, out_ch, ks, strd, pdd):
-    return nn.Sequential(
-        # 채널 증가
-        nn.Conv2d(in_ch, mid_ch, kernel_size=1, stride=1, padding=0, bias=False),
-        nn.BatchNorm2d(mid_ch),
-        nn.SiLU(),
-
-        # 채널별 형태 파악
-        nn.Conv2d(mid_ch, mid_ch, kernel_size=ks, stride=strd, padding=pdd, groups=mid_ch, bias=False),
-        nn.BatchNorm2d(mid_ch),
-        nn.SiLU(),
-
-        # 채널간 패턴 파악
-        nn.Conv2d(mid_ch, out_ch, kernel_size=1, stride=1, padding=0, bias=False),
-        nn.BatchNorm2d(out_ch),
-        nn.SiLU()
-    )
-
-
+# (EssenceNet 백본)
+# 기본 conv 로 흑백 1채널 이미지의 2차원 위치적 특징을 벡터 형태로 추출 하는 것에 집중
+# 좁은 범위 고해상도 -> 넓은 범위 저해상도로 피쳐 피라미드 반환
+# 글로벌 피쳐와 디테일 피쳐간 조합으로 모든 형태를 표현하는 개념(저해상도를 interpolate nearest 로 업스케일링하여 고해상도와 조립)
 class EssenceNet(nn.Module):
     def __init__(self):
         super().__init__()
-        # 멀티 스케일 + 복합 형태 정보
-        self.comp_feat_blocks = nn.ModuleList([
-            # 3x3 픽셀 단위 특징 검출
-            # 노이즈 + 점 + 에지
-            _double_conv_block(3, 32, 16, 3, 2, 1),  # 320x320 -> 160x160
-            # 7x7 픽셀 단위 특징 검출
-            # 노이즈 + 점 + 에지 + 직선 + 작은 곡선 = 픽셀 아이콘 영역
-            _mb_conv_block(16, 64, 32, 3, 2, 1),  # 160x160 -> 80x80
-            # 15x15 픽셀 단위 특징 검출
-            # 노이즈 + 점 + 에지 + 직선 + 곡선 + 패턴 = 픽셀 아이콘 영역
-            _mb_conv_block(32, 128, 64, 3, 2, 1),  # 80x80 -> 40x40
-            # 31x31 픽셀 단위 특징 검출
-            # 노이즈 + 점 + 에지 + 자유로운 선 + 패턴 + 제한된 도형 = 픽셀 아트 영역
-            _mb_conv_block(64, 256, 128, 3, 2, 1),  # 40x40 -> 20x20
-            # 63x63 픽셀 단위 특징 검출
-            # 노이즈 + 점 + 에지 + 자유로운 선 + 패턴 + 도형 = 픽셀 아트 영역
-            _mb_conv_block(128, 512, 256, 3, 2, 1),  # 20x20 -> 10x10
-            # 127x127 픽셀 단위 특징 검출
-            # 노이즈 + 점 + 에지 + 자유로운 선 + 패턴 + 자유로운 도형 + 질감 = 실사 이미지
-            _mb_conv_block(256, 1024, 512, 3, 2, 1),  # 10x10 -> 5x5
-            # 255x255 픽셀 단위 특징 검출
-            _mb_conv_block(512, 2048, 1024, 3, 2, 1),  # 5x5 -> 3x3
-            # 320x320 픽셀 단위 특징 검출
-            # 최고 추상 정보
-            _mb_conv_block(1024, 4096, 2048, 3, 1, 0)  # 3x3 -> 1x1
-        ])
 
-        # Post-BN + Activation 블록
-        self.post_bn_activations = nn.ModuleList()
-        # Residual 연결을 위한 1x1 conv 계층
-        self.residual_convs = nn.ModuleList()
-        self.residual_color_convs = nn.ModuleList()
-        for block in self.comp_feat_blocks:
-            conv_layers = [l for l in block.children() if isinstance(l, nn.Conv2d)]
-            in_ch = conv_layers[0].in_channels
-            out_ch = conv_layers[-1].out_channels
-            self.residual_convs.append(nn.Conv2d(in_ch, out_ch, kernel_size=1, bias=False))
-            self.residual_color_convs.append(nn.Conv2d(3, out_ch, kernel_size=1, bias=False))
-            self.post_bn_activations.append(nn.Sequential(nn.BatchNorm2d(out_ch), nn.SiLU()))
+        # 레이어 하나당 커널수(2배수)
+        self.exp_space = 64
 
-        num_blocks = len(self.comp_feat_blocks)
-        drop_probs = [float(i) / num_blocks * 0.2 for i in range(num_blocks)]
-        self.drop_paths = nn.ModuleList([StochasticDepth(p, mode='row') for p in drop_probs])
+        self.feat_blocks = [
+            (_single_conv_block(1, self.exp_space, 3, 1, 1), 256),
+            (_single_conv_block(1, self.exp_space, 3, 1, 1), 128),
+            (_single_conv_block(1, self.exp_space, 3, 1, 1), 64),
+            (_single_conv_block(1, self.exp_space, 3, 1, 1), 32),
+            (_single_conv_block(1, self.exp_space, 3, 1, 1), 16),
+            (_single_conv_block(1, self.exp_space, 3, 1, 1), 8),
+            (_single_conv_block(1, self.exp_space, 3, 1, 1), 4),
+            (_single_conv_block(1, self.exp_space, 3, 1, 1), 2),
+        ]
 
-        self.se_blocks = nn.ModuleList()
-        for block in self.comp_feat_blocks:
-            conv_layers = [l for l in block.children() if isinstance(l, nn.Conv2d)]
-            out_ch = conv_layers[-1].out_channels
-            self.se_blocks.append(SEBlock(out_ch))
+        # 3x3 -> 1x1
+        self.global_conv = _single_conv_block(1, self.exp_space, 3, 1, 0)
 
     def forward(self, x):
         assert x.shape[1] == 3, "Input tensor must have 3 channels (RGB)."
 
-        # 분석을 위한 흑백 변환(320x320)
-        # 컬러 이미지는 그 자체로 색이란 특징을 지닌 특징 맵이고, 형태 특징을 구하기 위한 입력 값은 흑백으로 충분
+        # 컬러 이미지
+        # (B, 3, 256, 256)
         color_feats = x
 
-        # 특징 저장 리스트(320, 160, 80, 40, 20, 10, 5, 3, 1 해상도 피라미트)
-        result_feats_list = [color_feats]  # 컬러 특징 저장
+        # 형태 분석을 위한 흑백 이미지
+        # (B, 1, 256, 256)
+        gray_feats = (
+            (color_feats * torch.tensor(
+                [0.299, 0.587, 0.114], device=color_feats.device).view(1, 3, 1, 1)
+             ).sum(dim=1, keepdim=True)
+        )
 
-        zipped = list(zip(
-            self.comp_feat_blocks,
-            self.residual_convs,
-            self.residual_color_convs,
-            self.drop_paths,
-            self.post_bn_activations,
-            self.se_blocks
-        ))
-        zipped_last_idx = len(zipped) - 1
+        # 특징 저장 리스트(사이즈 별 특징 피라미트, 전역 -> 지역)
+        result_feats_list = []
 
-        # 첫 입력값은 흑백 이미지
-        x_in = color_feats
-        for i, (
-                block,
-                res_conv,
-                res_color_conv,
-                drop_path,
-                post_bn_act,
-                se_block
-        ) in enumerate(zipped):
-            # 특징 추출
-            x_out = block(x_in)
+        # 멀티 스케일 특징 추출
+        # (B, self.exp_space, N, N)
+        for conv_block, size in self.feat_blocks:
+            # (B, self.exp_space, size, size)
+            conv_feats = conv_block(F.interpolate(gray_feats, size=(size, size), mode='area'))
+            # 채널 개수 : 3 + self.exp_space
+            conv_feats = torch.cat([F.interpolate(color_feats, size=(size, size), mode='area'), conv_feats], dim=1)
+            result_feats_list.append(conv_feats)
 
-            # 레이어 반환 특징맵 저장
-            result_feats_list.append(x_out)
-
-            if i == zipped_last_idx:
-                break
-
-            # 다음 특징 추출에 중요한 채널을 강조
-            x_out = se_block(x_out)
-
-            # 이전 결과값과 이번 결과값이 크게 나타나는 곳을 강조 및 역전파 지름길 만들기
-            if x_in.shape[2:] != x_out.shape[2:]:
-                # 부드러운 다운스케일링(평균값 사용)
-                x_in = F.interpolate(x_in, size=x_out.shape[2:], mode='area')
-
-            x_in = res_conv(x_in)  # 채널 크기 맞추기
-
-            # color_feats 를 Residual 에 추가하여 경로 축소 극대화 및 이전 레이어 분석시 제거된 특성 재입력
-            color_feats = F.interpolate(color_feats, size=x_out.shape[2:], mode='area')
-            if i > 0:
-                color_in = res_color_conv(color_feats)
-                x_in = drop_path(x_out + x_in + color_in)
-            else:
-                x_in = drop_path(x_out + x_in)
+        # global 특징 추출
+        # (B, self.exp_space, 1, 1)
+        conv_feats = self.global_conv(F.interpolate(gray_feats, size=(3, 3), mode='area'))
+        # 채널 개수 : 3 + self.exp_space
+        conv_feats = torch.cat([F.interpolate(color_feats, size=(1, 1), mode='area'), conv_feats], dim=1)
+        result_feats_list.append(conv_feats)
 
         return result_feats_list
 
 
 # ----------------------------------------------------------------------------------------------------------------------
+# todo : EssenceNet 에서 반환된 피쳐 피라미드를 어텐션으로 헤드 구성
 class EssenceNetClassifier(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
