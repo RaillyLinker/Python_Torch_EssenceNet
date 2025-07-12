4f92d56104616e68002dfd4d842690a82f96a65e
RaillyLinker <raillylinker@gmail.com>
2025-07-12 15:03:19 +0900

    update

--- nbb2.py
+++ nbb2.py
@@ -4,7 +4,6 @@ import torch.nn.functional as F
 from dropblock import DropBlock2D
 
 
-# todo : 1x1 conv 깊이 변경
 def _double_conv_block(in_ch, mid_ch, out_ch, ks, strd, pdd, dp, bs):
     return nn.Sequential(
         # 평면당 형태를 파악
@@ -30,7 +29,7 @@ class EssenceNet(nn.Module):
     def __init__(self):
         super().__init__()
 
-        # todo conv 채널 변경, 해상도를 2 배수(320)로 해보기
+        # todo conv 채널 변경, 해상도를 2 배수(320)로 해보기(단계가 많아지므로 residual 을 없애고, 특징맵 해상도 반토막으로 시작하기)
         self.feats_convs = nn.ModuleList([
             _double_conv_block(3, 32, 16, 3, 1, 1, 0.0, 1),  # 243x243 -> 243x243
             _double_conv_block(16, 64, 32, 3, 3, 0, 0.0, 1),  # 243x243 -> 81x81
@@ -40,7 +39,7 @@ class EssenceNet(nn.Module):
             _double_conv_block(256, 1024, 512, 3, 1, 0, 0.0, 1)  # 3x3 -> 1x1
         ])
 
-        # feats_convs를 기반으로 projection 레이어 자동 생성
+        # feats_convs 를 기반으로 projection 레이어 자동 생성
         self.projections = nn.ModuleList()
         prev_out_ch = 3  # 입력 이미지 채널 (RGB)
         for conv_block in self.feats_convs:
@@ -48,22 +47,19 @@ class EssenceNet(nn.Module):
             last_conv = conv_layers[-1]
             out_ch = last_conv.out_channels
 
-            if prev_out_ch == out_ch:
-                self.projections.append(nn.Identity())
-            else:
-                self.projections.append(
-                    nn.Sequential(
-                        nn.Conv2d(prev_out_ch, (prev_out_ch + out_ch) // 2, kernel_size=1, stride=1, bias=False),
-                        nn.BatchNorm2d((prev_out_ch + out_ch) // 2),
-                        nn.SiLU(),
+            self.projections.append(
+                nn.Sequential(
+                    nn.Conv2d(prev_out_ch, (prev_out_ch + out_ch) // 2, kernel_size=1, stride=1, bias=False),
+                    nn.BatchNorm2d((prev_out_ch + out_ch) // 2),
+                    nn.SiLU(),
 
-                        nn.Conv2d((prev_out_ch + out_ch) // 2, out_ch, kernel_size=1, stride=1, padding=0, bias=False),
-                        nn.BatchNorm2d(out_ch),
-                        nn.SiLU(),
+                    nn.Conv2d((prev_out_ch + out_ch) // 2, out_ch, kernel_size=1, stride=1, padding=0, bias=False),
+                    nn.BatchNorm2d(out_ch),
+                    nn.SiLU(),
 
-                        nn.Dropout2d(0.2)
-                    )
+                    nn.Dropout2d(0.2)
                 )
+            )
 
             prev_out_ch = out_ch
 
@@ -73,14 +69,20 @@ class EssenceNet(nn.Module):
 
         feat = x
         for idx, conv in enumerate(self.feats_convs):
+            # Residual 용 특징 저장
             identity = feat
+
+            # Conv 연산
             feat = conv(feat)
 
+            # Residual 해상도 맞추기
             if not isinstance(self.projections[idx], nn.Identity):
-                target_size = feat.shape[2:]
-                identity = F.interpolate(identity, size=target_size, mode='area')
+                identity = F.interpolate(identity, size=feat.shape[2:], mode='area')
 
+            # Residual 채널 맞추기
             projected = self.projections[idx](identity)
+
+            # Residual 합치기
             feat = feat + projected
             feats_list.append(feat)
 
@@ -88,9 +90,11 @@ class EssenceNet(nn.Module):
 
 
 # ----------------------------------------------------------------------------------------------------------------------
+# (Segment 모델)
 class EssenceNetSegmenter(nn.Module):
     def __init__(self, num_classes: int):
         super().__init__()
+        # conv 백본 모델
         self.backbone = EssenceNet()
 
         # 백본 특징맵 피라미드 채널 수
@@ -98,7 +102,6 @@ class EssenceNetSegmenter(nn.Module):
         self.encoder_input = sum(self.feat_channels)
 
         # 분류기 헤드
-        # todo : 1x1 conv 깊이 변경
         self.classifier_head = nn.Sequential(
             nn.Conv2d(self.encoder_input, (self.encoder_input + num_classes) // 2, kernel_size=1, bias=False),
             nn.BatchNorm2d((self.encoder_input + num_classes) // 2),
@@ -110,12 +113,13 @@ class EssenceNetSegmenter(nn.Module):
         )
 
     def forward(self, x):
+        # 백본 특징맵 피라미드 추출
         feats = self.backbone(x)
 
-        target_size = feats[0].shape[2:]
+        # 특징맵 피라미드들을 최고 해상도 기준으로 합치기
+        concat_feats = torch.cat([F.interpolate(f, size=feats[0].shape[2:], mode='nearest') for f in feats], dim=1)
 
-        concat_feats = torch.cat([F.interpolate(f, size=target_size, mode='nearest') for f in feats], dim=1)
-
+        # 픽셀별 분류 헤드 적용
         logits = self.classifier_head(concat_feats)
 
         return logits
