c1e61f9900f2f6690e10adbe7bcefae456c313fb
RaillyLinker <raillylinker@gmail.com>
2025-07-15 19:03:20 +0900

    update

--- nbb2.py
+++ nbb2.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 from dropblock import DropBlock2D
 
 
+# todo : conv 블록을 깊게할지, conv 채널을 크게 할지, head 를 깊게 할지
 def _double_conv_block(in_ch, mid_ch, out_ch, ks, strd, pdd, dp, bs):
     hidden_dim = (mid_ch + out_ch) // 2
     return nn.Sequential(
@@ -25,47 +26,47 @@ def _double_conv_block(in_ch, mid_ch, out_ch, ks, strd, pdd, dp, bs):
     )
 
 
-# todo 백본을 굵게 할지, 헤드를 깊게 할지
 # (EssenceNet 백본)
 class EssenceNet(nn.Module):
     def __init__(self):
         super().__init__()
 
-        # todo conv 채널 변경
+        # todo : 모두 동일한 크기로 해보기(3x3 이 할 수 있는 최대 크기로 판단)
         self.feats_convs = nn.ModuleList([
-            _double_conv_block(3, 64, 32, 3, 2, 1, 0.0, 1),  # 320x320 -> 160x160
-            _double_conv_block(32, 128, 32, 3, 2, 1, 0.05, 3),  # 160x160 -> 80x80
-            _double_conv_block(32, 128, 64, 3, 2, 1, 0.10, 3),  # 80x80 -> 40x40
-            _double_conv_block(64, 256, 64, 3, 2, 1, 0.15, 5),  # 40x40 -> 20x20
-            _double_conv_block(64, 256, 128, 3, 2, 1, 0.20, 5),  # 20x20 -> 10x10
-            _double_conv_block(128, 512, 128, 3, 2, 1, 0.20, 3),  # 10x10 -> 5x5
-            _double_conv_block(128, 512, 256, 3, 2, 1, 0.15, 3),  # 5x5 -> 3x3
-            _double_conv_block(256, 1024, 256, 3, 1, 0, 0.0, 1)  # 3x3 -> 1x1
+            _double_conv_block(3, 128, 64, 3, 2, 1, 0.0, 1),  # 320x320 -> 160x160
+            _double_conv_block(64, 256, 64, 3, 2, 1, 0.05, 3),  # 160x160 -> 80x80
+            _double_conv_block(64, 256, 128, 3, 2, 1, 0.10, 3),  # 80x80 -> 40x40
+            _double_conv_block(128, 512, 128, 3, 2, 1, 0.15, 5),  # 40x40 -> 20x20
+            _double_conv_block(128, 512, 256, 3, 2, 1, 0.20, 5),  # 20x20 -> 10x10
+            _double_conv_block(256, 1024, 256, 3, 2, 1, 0.20, 3),  # 10x10 -> 5x5
+            _double_conv_block(256, 1024, 512, 3, 2, 1, 0.15, 3),  # 5x5 -> 3x3
+            _double_conv_block(512, 2048, 512, 3, 1, 0, 0.0, 1)  # 3x3 -> 1x1
         ])
 
+        # todo : residual 지워보기
         # feats_convs 를 기반으로 projection 레이어 자동 생성
-        self.projections = nn.ModuleList()
-        prev_out_ch = 3  # 입력 이미지 채널 (RGB)
-        for conv_block in self.feats_convs:
-            conv_layers = [layer for layer in conv_block.modules() if isinstance(layer, nn.Conv2d)]
-            last_conv = conv_layers[-1]
-            out_ch = last_conv.out_channels
-
-            self.projections.append(
-                nn.Sequential(
-                    nn.Conv2d(prev_out_ch, (prev_out_ch + out_ch) // 2, kernel_size=1, stride=1, bias=False),
-                    nn.BatchNorm2d((prev_out_ch + out_ch) // 2),
-                    nn.SiLU(),
-
-                    nn.Conv2d((prev_out_ch + out_ch) // 2, out_ch, kernel_size=1, stride=1, padding=0, bias=False),
-                    nn.BatchNorm2d(out_ch),
-                    nn.SiLU(),
-
-                    nn.Dropout2d(0.2)
-                )
-            )
-
-            prev_out_ch = out_ch
+        # self.projections = nn.ModuleList()
+        # prev_out_ch = 3  # 입력 이미지 채널 (RGB)
+        # for conv_block in self.feats_convs:
+        #     conv_layers = [layer for layer in conv_block.modules() if isinstance(layer, nn.Conv2d)]
+        #     last_conv = conv_layers[-1]
+        #     out_ch = last_conv.out_channels
+        #
+        #     self.projections.append(
+        #         nn.Sequential(
+        #             nn.Conv2d(prev_out_ch, (prev_out_ch + out_ch) // 2, kernel_size=1, stride=1, bias=False),
+        #             nn.BatchNorm2d((prev_out_ch + out_ch) // 2),
+        #             nn.SiLU(),
+        #
+        #             nn.Conv2d((prev_out_ch + out_ch) // 2, out_ch, kernel_size=1, stride=1, padding=0, bias=False),
+        #             nn.BatchNorm2d(out_ch),
+        #             nn.SiLU(),
+        #
+        #             nn.Dropout2d(0.2)
+        #         )
+        #     )
+        #
+        #     prev_out_ch = out_ch
 
     def forward(self, x):
         assert x.shape[1] == 3, "Input tensor must have 3 channels (RGB)."
@@ -73,21 +74,21 @@ class EssenceNet(nn.Module):
 
         feat = x
         for idx, conv in enumerate(self.feats_convs):
-            # Residual 용 특징 저장
-            identity = feat
+            # # Residual 용 특징 저장
+            # identity = feat
 
             # Conv 연산
             feat = conv(feat)
 
-            # Residual 해상도 맞추기
-            if not isinstance(self.projections[idx], nn.Identity):
-                identity = F.interpolate(identity, size=feat.shape[2:], mode='area')
-
-            # Residual 채널 맞추기
-            projected = self.projections[idx](identity)
-
-            # Residual 합치기
-            feat = feat + projected
+            # # Residual 해상도 맞추기
+            # if not isinstance(self.projections[idx], nn.Identity):
+            #     identity = F.interpolate(identity, size=feat.shape[2:], mode='area')
+            #
+            # # Residual 채널 맞추기
+            # projected = self.projections[idx](identity)
+            #
+            # # Residual 합치기
+            # feat = feat + projected
 
             # 특징 저장
             feats_list.append(feat)
@@ -104,29 +105,13 @@ class EssenceNetSegmenter(nn.Module):
         self.backbone = EssenceNet()
 
         # 백본 특징맵 피라미드 채널 수
-        self.feat_channels = [32, 32, 64, 64, 128, 128, 256, 256]
+        self.feat_channels = [64, 64, 128, 128, 256, 256, 512, 512]
         self.encoder_input = sum(self.feat_channels)
 
         # 분류기 헤드
         hidden_dim = (self.encoder_input + num_classes) // 2
         self.classifier_head = nn.Sequential(
             nn.Conv2d(self.encoder_input, hidden_dim, kernel_size=1, bias=False),
-            nn.BatchNorm2d(hidden_dim),
-            nn.SiLU(),
-
-            nn.Conv2d(hidden_dim, hidden_dim, kernel_size=1, bias=False),
-            nn.BatchNorm2d(hidden_dim),
-            nn.SiLU(),
-
-            nn.Conv2d(hidden_dim, hidden_dim, kernel_size=1, bias=False),
-            nn.BatchNorm2d(hidden_dim),
-            nn.SiLU(),
-
-            nn.Conv2d(hidden_dim, hidden_dim, kernel_size=1, bias=False),
-            nn.BatchNorm2d(hidden_dim),
-            nn.SiLU(),
-
-            nn.Conv2d(hidden_dim, hidden_dim, kernel_size=1, bias=False),
             nn.BatchNorm2d(hidden_dim),
             nn.SiLU(),
 
