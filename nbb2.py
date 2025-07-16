eb2671141c4a274584feeb6b8483dc3dde78229f
RaillyLinker <raillylinker@gmail.com>
2025-07-16 14:45:00 +0900

    update

--- nbb2.py
+++ nbb2.py
@@ -38,12 +38,12 @@ class EssenceNet(nn.Module):
 
         self.feats_convs = nn.ModuleList([
             _single_conv_block(3, 24, 3, 2, 1, 0.0, 1),  # 320x320 -> 160x160
-            _double_conv_block(24, 96, 64, 3, 2, 1, 0.05, 3),  # 160x160 -> 80x80
-            _double_conv_block(64, 128, 96, 3, 2, 1, 0.10, 3),  # 80x80 -> 40x40
-            _double_conv_block(96, 192, 128, 3, 2, 1, 0.15, 5),  # 40x40 -> 20x20
-            _double_conv_block(128, 256, 192, 3, 2, 1, 0.20, 5),  # 20x20 -> 10x10
-            _double_conv_block(192, 384, 256, 3, 2, 1, 0.20, 3),  # 10x10 -> 5x5
-            _double_conv_block(256, 512, 384, 3, 2, 1, 0.15, 3),  # 5x5 -> 3x3
+            _double_conv_block(24, 128, 64, 3, 2, 1, 0.05, 3),  # 160x160 -> 80x80
+            _double_conv_block(64, 192, 96, 3, 2, 1, 0.10, 3),  # 80x80 -> 40x40
+            _double_conv_block(96, 256, 128, 3, 2, 1, 0.15, 5),  # 40x40 -> 20x20
+            _double_conv_block(128, 384, 192, 3, 2, 1, 0.20, 5),  # 20x20 -> 10x10
+            _double_conv_block(192, 512, 256, 3, 2, 1, 0.20, 3),  # 10x10 -> 5x5
+            _double_conv_block(256, 768, 384, 3, 2, 1, 0.15, 3),  # 5x5 -> 3x3
             _double_conv_block(384, 1024, 512, 3, 1, 0, 0.0, 1)  # 3x3 -> 1x1
         ])
 
@@ -54,14 +54,6 @@ class EssenceNet(nn.Module):
         self.encoder_output = encoder_input // 2
         self.encoder_head = nn.Sequential(
             nn.Conv2d(encoder_input, self.encoder_output, kernel_size=1, bias=False),
-            nn.BatchNorm2d(self.encoder_output),
-            nn.SiLU(),
-
-            nn.Conv2d(self.encoder_output, self.encoder_output, kernel_size=1, bias=False),
-            nn.BatchNorm2d(self.encoder_output),
-            nn.SiLU(),
-
-            nn.Conv2d(self.encoder_output, self.encoder_output, kernel_size=1, bias=False),
             nn.BatchNorm2d(self.encoder_output),
             nn.SiLU(),
 
