0d00924d380f81be12688c893fb1c7b5a09f465c
RaillyLinker <raillylinker@gmail.com>
2025-07-18 16:49:07 +0900

    update

--- segmentation_nbb.py
+++ segmentation_nbb.py
@@ -4,7 +4,6 @@ import torch.nn.functional as F
 from dropblock import DropBlock2D
 
 
-# todo : 다음 실험 :  conv 증량
 class Swish(nn.Module):
     def forward(self, x):
         return x * torch.sigmoid(x)
@@ -40,14 +39,9 @@ class EssenceNet(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.to_gray = nn.Sequential(
-            # 평면당 형태를 파악
-            nn.Conv2d(3, 1, kernel_size=1, stride=1, padding=0, bias=False),
-            nn.BatchNorm2d(1)
-        )
-
+        # todo : 증량해보기
         self.feats_convs = nn.ModuleList([
-            _single_conv_block(1, 16, 3, 2, 1),  # 320x320 -> 160x160
+            _single_conv_block(3, 16, 3, 2, 1),  # 320x320 -> 160x160
             _double_conv_block(16, 48, 24, 3, 2, 1, 0.05, 3),  # 160x160 -> 80x80
             _double_conv_block(24, 64, 32, 3, 2, 1, 0.10, 3),  # 80x80 -> 40x40
             _double_conv_block(32, 96, 48, 3, 2, 1, 0.15, 5),  # 40x40 -> 20x20
@@ -58,21 +52,16 @@ class EssenceNet(nn.Module):
         ])
 
         # 특징맵 피라미드 채널 수
-        def get_last_conv_out_channels(block: nn.Module) -> int:
-            for layer in reversed(list(block.children())):
-                if isinstance(layer, nn.Conv2d):
-                    return layer.out_channels
-            raise ValueError("No Conv2d layer found in block")
-
         encoder_input = sum([
-            get_last_conv_out_channels(conv)
+            (lambda block: next(
+                layer.out_channels for layer in reversed(list(block.children())) if isinstance(layer, nn.Conv2d)))(conv)
             for conv in self.feats_convs
         ])
 
         # 인코더 헤드
         self.encoder_output = 1280
         self.encoder_head = nn.Sequential(
-            nn.Conv2d(encoder_input + 3, self.encoder_output, kernel_size=1, bias=False),
+            nn.Conv2d(encoder_input, self.encoder_output, kernel_size=1, bias=False),
             nn.BatchNorm2d(self.encoder_output),
             Swish(),
 
@@ -83,11 +72,8 @@ class EssenceNet(nn.Module):
         assert x.shape[1] == 3, "Input tensor must have 3 channels (RGB)."
         feats_list = []
 
-        # 순수한 형태 분석을 위한 흑백 변환
-        gray_feats = self.to_gray(x)
-
         # conv 형태 분석
-        feat = gray_feats
+        feat = x
         for idx, conv in enumerate(self.feats_convs):
             # Conv 연산
             feat = conv(feat)
@@ -95,14 +81,12 @@ class EssenceNet(nn.Module):
             # 특징 저장
             feats_list.append(feat)
 
-        # 특징맵 피라미드 업샘플링
-        shape_feats = [F.interpolate(f, size=feats_list[0].shape[2:], mode='nearest') for f in feats_list]
-
-        # 색 특징 다운 샘플링
-        color_feats = F.adaptive_avg_pool2d(x, output_size=(shape_feats[0].shape[2], shape_feats[0].shape[3]))
-
-        # 색 특징 + 형태 특징 결합
-        concat_feats = torch.cat([color_feats, *shape_feats], dim=1)
+        # TODO : 합치며 역순으로 1x1 conv 돌려보기(FPN 처럼)
+        # 특징맵 피라미드 업샘플링 및 결합
+        concat_feats = torch.cat(
+            [F.interpolate(f, size=feats_list[0].shape[2:], mode='nearest') for f in feats_list],
+            dim=1
+        )
 
         # 이미지 특징 최종 Projection
         return self.encoder_head(concat_feats)
